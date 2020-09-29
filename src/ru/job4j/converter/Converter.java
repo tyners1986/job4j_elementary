fe5bb0c2187138e97ff178f4f117861217640ae6
veldi <shopsed@mail.ru>
2020-09-29 20:00:58 +0300

    Конвертер валюты [#370084]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,20 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static int euroToRub(int value) {
+        int euro = value * 70;
+        return euro;
+    }
+    public static int dollarToRub(int value) {
+        int dollar = value * 60;
+        return dollar;
+            }
+
+    public static void main(String[] args) {
+        int euro = Converter.euroToRub(1);
+        System.out.println("1 euro " + euro + " rub.");
+        int dollar = Converter.dollarToRub(1);
+        System.out.println("1 dollar " + dollar + " rub.");
+    }
+}
