f6aa10ee09369ef38d2d0515259f4105ac52b320
veldi <shopsed@mail.ru>
2020-10-03 07:27:27 +0300

    1.1. Входные данные в тесте [#370122]

--- test/ru/job4j/calculate/X2Test.java
+++ test/ru/job4j/calculate/X2Test.java
@@ -46,13 +46,13 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
     @Test
-    public void whenA1B1C1X0Then2() {
+    public void whenA1B1C1X0Then1() {
 
         int a = 1;
         int b = 1;
-        int c = 0;
-        int x = 1;
-        int expected = 2;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
 
         int rsl = X2.calc(a, b, c, x);
 
