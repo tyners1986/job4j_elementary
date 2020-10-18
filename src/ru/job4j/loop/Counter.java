b6e7849b25e5024431c229f13783f5fbf8b79382
veldi <shopsed@mail.ru>
2020-10-18 18:50:03 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#370149]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -19,4 +19,5 @@ public class Counter {
         return sum;
     }
 
+
 }
--- test/ru/job4j/loop/CounterTest.java
+++ test/ru/job4j/loop/CounterTest.java
@@ -13,4 +13,10 @@ public class CounterTest {
         int expected = 30;
         assertThat(rsl, is(expected));
     }
+    @Test
+    public void NumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 20);
+        int expected = 110;
+        assertThat(rsl, is(expected));
+    }
 }
