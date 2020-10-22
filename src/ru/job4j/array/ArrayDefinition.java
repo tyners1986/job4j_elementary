844afd1a77f9d9acc3fb0e58a56b34a4dd8d3bc1
veldi <shopsed@mail.ru>
2020-10-22 15:49:23 +0300

    6.0.1. Объявление массива. [#370081]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ades = new short[10];
+        String[] suenames = new String[100500];
+        float[] prices = new float[40];
+
+    }
+}
