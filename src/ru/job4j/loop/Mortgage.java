70e0a9a0b31eecb2d588713a0e21b396650adf00
veldi <shopsed@mail.ru>
2020-10-22 10:22:56 +0300

    5.7. Ипотека [#370143]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+
+
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        while (0 <= amount) {
+            amount += amount * percent / 100;
+            amount -= salary;
+
+            year++;
+        }
+        return year;
+    }
+}
