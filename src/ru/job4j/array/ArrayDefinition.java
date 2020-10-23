c2b3d1c6758a54d21923de813cd720fb027c0f57
veldi <shopsed@mail.ru>
2020-10-23 09:17:47 +0300

    6.0.1.1. Размер массива [#370077]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,7 @@ public class ArrayDefinition {
         short[] ades = new short[10];
         String[] suenames = new String[100500];
         float[] prices = new float[40];
-
+        System.out.println("Размер массива равен: " + suenames.length);
     }
+
 }
