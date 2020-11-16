6a642f9ae993526fb428702102810a5b9edb314c
veldi <shopsed@mail.ru>
2020-11-16 20:30:07 +0300

    3. Объединить два отсортированных массива

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,21 +3,31 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int i = 0;
-        int j = 0;
-        int z = 0;
-        while (i < left.length && j < right.length) {
-            rsl[z++] = left[i] < right[j] ? left[i++] : right[j++];
-        }
-        while (i < left.length) {
-            rsl[z++] = left[i++];
+        int i = 0, j = 0;
+        for (int z = 0; z < rsl.length; z++) {
 
+            if (i > left.length - 1) {
+                int a = right[j];
+                rsl[z] = a;
+                j++;
+            }
+            else if (j > right.length - 1) {
+                int a = left[i];
+                rsl[z] = a;
+                i++;
+            }
+            else if (left[i] < right[j]) {
+                int a = left[i];
+                rsl[z] = a;
+                i++;
+            }
+            else {
+                int b = right[j];
+                rsl[z] = b;
+                j++;
+            }
         }
-        while (j < right.length) {
-            rsl[z++] = right[j++];
 
-
-        }
 
 
         return rsl;
